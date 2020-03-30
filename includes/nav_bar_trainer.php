5deca9e8cbbfb999d522ae3df11c744cdb23e0ac
Oana Lung <oanalung14@yahoo.com>
2020-03-30 19:31:16 +0300

    T5-refactored landing page, created file structure for trainer

--- includes/nav_bar_trainer.php
+++ includes/nav_bar_trainer.php
@@ -1,7 +1,22 @@
+<?php
+@session_start();
+$user_id = $_SESSION['userData']['id'];
+$userDetail = new UserDetail();
+$userDetail = $userDetail->where('id_user', $user_id)->firstOrFail();
+?>
 <!-- Navbar -->
 <ul class="nav nav-tabs">
   <li class="nav-item">
-    <a class="nav-link active" href="#">Home</a>
+    <a class="nav-link active" href="http://localhost/trainingApp/index_trainer.php">Training management</a>
   </li>
-  <?php require_once('account_bar.php') ?>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $userDetail->first_name . ' ' . $userDetail->last_name ?></a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="http://localhost/trainingApp/createTraining.php">Create training</a>
+            <a class="dropdown-item" href="http://localhost/trainingApp/trainer/joinTraining.php">Join training</a>
+            <a class="dropdown-item" href="http://localhost/trainingApp/trainer/trainerProfile.php">Profile</a>
+            <a class="dropdown-item" href="http://localhost/trainingApp/trainer/trainerStatistics.php">Statistics</a>
+            <a class="dropdown-item" href="logout.php">Logout</a>
+        </div>
+    </li>
 </ul>
