dd24627345ffabb0f29fb7358daa1fcba1e60434
NicoViregan <nico_viregan@yahoo.com>
2020-03-31 22:42:04 +0300

    added functionality for user's trainings visualization

--- entities/user_training.php
+++ entities/user_training.php
@@ -4,4 +4,22 @@ use Illuminate\Database\Eloquent\Model;
 class UserTraining extends Model {
 	protected $table = 'user_training';
 	public $timestamps = NULL;
+
+	public function isAlreadyEnrolled($userId, $trainingId) {
+	    return $this->where('id_user', $userId)->where('id_training', $trainingId)->count() != 0;
+    }
+
+    public function getEnrolled($userId) {
+	    $allTrainings = $this->select('id_training')->where('id_user', $userId)->get();
+
+	    $trainingIds = [];
+	    foreach ($allTrainings as $t){
+	        $trainingIds[]=$t->id_training;
+        }
+	    return $trainingIds;
+    }
+
+    public function getEnrolledCount($trainingId) {
+        return $this->where('id_training', $trainingId)->count();
+    }
 }
