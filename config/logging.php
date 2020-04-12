61407cca9af88647104236fe40064d9baa50aad0
Oana Lung <oanalung14@yahoo.com>
2020-04-12 22:16:58 +0300

    use errorlog

---
+++ config/logging.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+        ],
+        'single' => [
+            'driver' => 'errorlog',
+            'level' => 'debug',
+        ]
+]];
